5d60cda5ec9ab022ddac84bb2a52af8331dacc7c
Dorian Kita <dorian.kita90@gmail.com>
2024-11-19 19:04:51 +0100

    created QuizBrain class with method and attributes

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
     new_question = Question(question['text'], question['answer'])
     question_bank.append(new_question)
 
-print(question_bank[0].answer)
+
+question = QuizBrain(question_bank)
+
+
+question.next_question()
